7d596b05f4a45d064085efd6f4746813b4157f22
ShannEstabillo <u6510099@au.edu>
2025-06-15 23:36:36 +0700

    Add files via upload

--- petshop/loadpets.js
+++ petshop/loadpets.js
@@ -3,7 +3,12 @@ const pets = [
   {"name": "Buddy", "type": "Dog", "age": 3, "img": "petshop-img/petshop-img/dogs/dog02.jpg"}, 
   {"name": "Whiskers", "type": "Cat", "age": 2, "img": "petshop-img/petshop-img/cats/cat01.jpg"}, 
   {"name": "Mittens", "type": "Cat", "age": 2, "img": "petshop-img/petshop-img/cats/cat02.jpg"}, 
-
+  {"name": "Marley", "type": "Dog", "age": 2, "img": "petshop-img/petshop-img/dogs/dog03.jpg"},
+  {"name": "Sadie", "type": "Cat", "age": 1, "img": "petshop-img/petshop-img/cats/cat03.jpg"},
+  {"name": "Henry", "type": "Capybara", "age": 3, "img": "petshop-img/petshop-img/capybaras/capybara01.jpg"},
+  {"name": "Charles", "type": "Capybara", "age": 3, "img": "petshop-img/petshop-img/capybaras/capybara02.jpg"},
+  {"name": "Tilly", "type": "Bird", "age": 1, "img": "petshop-img/petshop-img/birds/bird01.jpg"},
+  {"name": "Blu", "type": "Bird", "age": 2, "img": "petshop-img/petshop-img/birds/bird02.jpg"},
 ] 
 
 function loadPets() { 
